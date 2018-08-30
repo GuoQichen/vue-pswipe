d783bd30e62dd4d7c9987d2cbd6c5e55a4228e62
GuoQichen <250915274@qq.com>
2018-08-30 23:53:33 +0800

    refactor: modify globaltion inject

--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
 import Photoswipe from './components/photoswipe.vue'
 import ImageItem from './components/imageItem.vue'
 
+const getGlobalMixin = options => ({
+    data() {
+        return {
+            globalOptions: options,
+        }
+    },
+})
+
 export default {
     install(Vue, options) {
         Vue.component('Photoswipe', {
-            data() {
-                return {
-                    globalOptions: options,
-                }
-            },
+            mixins: [getGlobalMixin(options)],
             ...Photoswipe,
         })
         Vue.component('PhotoswipeItem', ImageItem)
